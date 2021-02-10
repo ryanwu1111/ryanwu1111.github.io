f1ad4d552ce5e5bb61fca2ed1b5caf0a63ab8780
ryanwu1111 <71023642+ryanwu1111@users.noreply.github.com>
2021-02-10 12:40:47 +0400

    Replacing all files for 30 mmf

---
+++ Home Page/script.js
@@ -0,0 +1,10 @@
+let myVariable;
+
+myVariable="This js file works!";
+console.log(myVariable);
+
+let aboutUs = document.getElementsByClassName('navigation-1');
+
+function myFunction(x) {
+  x.classList.toggle("fa-thumbs-down");
+}
